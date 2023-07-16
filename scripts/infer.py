24a9967ea4a84cfef73eba36d4beb0340e960ff4
lianghaitian <tian@lianghaitiandeMacBook-Air.local>
2023-07-16 19:11:22 +0800

    addinfer

--- scripts/infer.py
+++ scripts/infer.py
@@ -16,7 +16,7 @@ from core.models import get_model
 
 def infer(image):
     # print(args.__dict__)
-    {'model': 'deeplabv3_resnet50_voc', 'dataset': 'pascal_aug', 'save_folder': '~/.torch/models', 
+    args= {'model': 'deeplabv3_resnet50_voc', 'dataset': 'pascal_aug', 'save_folder': '~/.torch/models', 
      'input_pic': '../datasets/voc/VOC2012/JPEGImages/2007_000032.jpg', 'outdir': './eval', 'local_rank': 0, 'aux': False}
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
